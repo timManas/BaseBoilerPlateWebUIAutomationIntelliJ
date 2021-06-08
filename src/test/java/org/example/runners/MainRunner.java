78798c776530349876d95bc258c7a0155b5faf0c
Tim M <timothy.romero.manas@gmail.com>
2021-06-08 19:10:32 -0400

    ABLE TO FINALLY EXECUTE on INTELLIJ for JUnit

--- src/test/java/org/example/runners/MainRunner.java
+++ src/test/java/org/example/runners/MainRunner.java
@@ -1,27 +1,30 @@
 package org.example.runners;
 
+import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.class) // Comment this out for testng
 
 @CucumberOptions (
-        features = {"/Users/timmanas/ProgrammingProjects/Java/BaseBoilerPlateWebUIAutomation/CucumberFramework/src/test/java/CucumberFramework/featureFiles"},
+        features = {"/Users/timmanas/ProgrammingProjects/Java/BaseBoilerPlateWebUIAutomation/CucumberFramework/src/test/java/CucumberFramework/featureFiles/"},
         glue = {"org.example.steps"},
         monochrome = true,
-        tags = "",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json"
         }
 )
 
-// UnComment this when running with TestNG
+// Uncomment htis when running with JUnit
+public class MainRunner {
+
+}
+
+//// UnComment this when running with TestNG
 //public class MainRunner extends AbstractTestNGCucumberTests {
 //}
 
-// Uncomment htis when running with JUnit
-public class MainRunner {
-}
+
 
